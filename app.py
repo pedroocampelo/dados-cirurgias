1e9fdc5365c9460a7e1f3c2e5e088cc0cdb25d20
pedroocampelo <pedrocampelo.fgv@gmail.com>
2026-01-24 13:19:47 -0300

    app.py

--- app.py
+++ app.py
@@ -644,9 +644,9 @@ modo = st.radio(
 )
 
 if modo == "Média do agregado":
-    show(fig_mensal_media_agregado(df_base), height=850)
+    show(fig_mensal_media_agregado(df_base), height=650)
 else:
-    show(fig_mensal_por_ano(df_base), height=900)
+    show(fig_mensal_por_ano(df_base), height=650)
 
 st.caption(
     "Os dados mensais consideram cirurgias a partir de maio de 2021. "
@@ -661,23 +661,23 @@ cc1, cc2 = st.columns(2)
 with cc1:
     show(donut_sexo(df), height=650)
 with cc2:
-    show(barh_faixa(df), height=750)
-show(idade_histograma_barras(df, bin_size=5), height=850)
+    show(barh_faixa(df), height=700)
+show(idade_histograma_barras(df, bin_size=5), height=650)
 
 st.markdown("<hr/>", unsafe_allow_html=True)
 
 st.header("Distribuição das técnicas cirúrgicas")
-show(bar_tecnica_vertical(df), height=750)
+show(bar_tecnica_vertical(df), height=650)
 
 st.markdown("<hr/>", unsafe_allow_html=True)
 
 st.header("Convênios")
-show(barh_convenio_top5_outros(df), height=750)
+show(barh_convenio_top5_outros(df), height=700)
 
 st.markdown("<hr/>", unsafe_allow_html=True)
 
 st.header("Hospitais")
-show(barh_hospital(df), height=750)
+show(barh_hospital(df), height=700)
 
 st.markdown("<hr/>", unsafe_allow_html=True)
 
